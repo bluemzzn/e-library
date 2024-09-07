88d697ea223a348be19414f74060fcfdf84ccd53
bluemzzn <synthiawithsyren@gmail.com>
2024-09-07 12:52:08 +0700

    Add files via upload

---
+++ detail.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="styles.css">
+	<title>Detail Product</title>
+</head>
+<body>
+  <style type="text/css">
+    body{
+      background-color: #f1f1f1;
+    }
+  </style>
+	<div id="mainwrapper">
+<header> 
+    <div id="logo"><a href="#"><img src="pic/logo.png" width="50px" height="50px">ระบบห้องสมุดอิเล็กทรอนิกส์</a></div>
+    <nav><a href="index.php" title="Link">&#127968 Home</a> 
+    	<a href="login.php" title="Link">&#128274 Login</a>
+    <a href="signup.php" title="Link">&#128272 Signup</a> </nav>
+  </header>
+  </header>
+   <div class="container"> 
+    <section class="prodetail">
+     
+      <div class="imgde">
+        <img src="pic/book.png">
+      </div>
+
+      <div class="infode">
+        <h1>หนังสือคณิตศาสตร์จุ๊กกรู้วววววววว</h1> 
+        <p>ผู้แต่ง : นิ้วเหลี่ยม</p>
+        <h2>352.00 B</h2>
+        <h3>Product Details</h3>
+        <span>หนังสือการบัญชีเพื่อการจัดการ (พิมพ์ครั้งที่ 3) เล่มนี้เขียนขึ้นมาเพื่อใช้ประกอบการเรียนการสอนในวิชาการบัญชีเพื่อการจัดการตามหลักสูตรปริญญาตรี และนอกจากนี้ยังเป็นการเผยแพร่ความรู้ไปสู่สาธารณชนทั่วไปอีกด้วย ในหนังสือเล่มนี้ได้อธิบายหลักการที่สำคัญเกี่ยวกับการใช้ประโยชน์จากข้อมูลทางการบัญชีเพื่อการจัดการอย่างมีประสิทธิภาพและประสิทธิผล เนื้อหาในแต่ละบทประกอบด้วย วัตถุประสงค์ของการศึกษา บทนำ เนื้อหา สรุป และแบบฝึกหัดท้ายบท ภายหลังการศึกษาหนังสือเล่มนี้ ท่านจะตระหนักถึงคุณค่าของข้อมูลและความสำคัญของการตัดสินใจภายใต้ข้อมูลที่เหมาะสม ตลอดจนท่านสามารถปฏิบัติงานในหน้าที่การวางแผน การควบคุม และการตัดสินใจได้อย่างถูกต้องและรวดเร็ว อันจะส่งผลให้ท่านประสบความสำเร็จในการบริหารงาน</span>
+        <div class="keyword">
+        <h3> Keyword :</h3>
+        <a href="index.php?search=500คณิต" >500 วิทยาศาสตร์และคณิตศาสตร์</a>
+        </div>
+      </div>
+    </section>
+
+    <section class="print">
+      <p>หมวดหมู่ : 000 หนังสือทั่วไป</p>
+      <p>สำนักพิมพ์ : ไทยกิงโกะ</p>
+      <p>วันที่จัดพิมพ์ : 6</p>
+      <p>ครั้งที่พิมพ์ : 5</p>
+    </section>
+    </div>
+  <footer>
+    <div id="footerbar">Copyright @SWK School.</div>
+  </footer>
+</body>
+</html>
+
+<?php   
+
+ ?>
