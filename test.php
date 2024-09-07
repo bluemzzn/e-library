8df8d8b1ad550e54d305c2c010c30baa53a358b9
bluemzzn <synthiawithsyren@gmail.com>
2024-09-07 15:01:11 +0700

    Add files via upload

---
+++ test.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="styles.css">
+	<title>Home</title>
+</head>
+<body>
+	<div id="mainwrapper">
+  <header> 
+    <div id="logo"><a href="#"><img src="pic/logo.png" width="50px" height="50px">ระบบห้องสมุดอิเล็กทรอนิกส์</a></div>
+    <nav> <a href="index.php" title="Link">&#127968 Home</a> 
+    	<a href="login.php" title="Link">&#128274 Login</a>
+    	<a href="signup.php" title="Link">&#128272 Signup</a> </nav>
+  </header>
+</div>
+</body>
+</html>
